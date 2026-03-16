63102369576fbd3982b47f76891ac34cefa66079
Joseph Rivera <rmroot@murena.io>
2026-03-15 23:08:28 -0400

    Add files via upload

---
+++ antiadhd/AntiADHD_LinuxWayland.sh
@@ -0,0 +1,1 @@
+# WAYLAND VERSION because wayland has lots of security which brings up lots of unsupported stuff.
---
+++ antiadhd/antiadhdinstaller.py
@@ -0,0 +1,6 @@
+# cross platform installer so its smoooooth
+import os
+installpyfig = input("do you have pyfiglet? (y/n):")
+if installpyfig: y:
+    print("installing pyfiglet...")
+    os.system("pip install pyfiglet --user")
